50bd3afca2d898b17577ce75febc68acf79c5583
Shivam Kumar <Shivamkumargupta07@gmail.com>
2023-10-19 00:08:11 +0530

    Adding inMemoryAuthentication config

---
+++ src/main/java/com/spring/security/example/SpringSecurityExample/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.spring.security.example.SpringSecurityExample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userOne = User.builder()
+                .username("shivam")
+                .password(passwordEncoder().encode("test"))
+                .roles("ADMIN").build();
+
+        UserDetails userTwo = User.builder()
+                .username("satyam")
+                .password(passwordEncoder().encode("test"))
+                .roles("ADMIN").build();
+
+        UserDetails userThree = User.builder()
+                .username("sundram")
+                .password(passwordEncoder().encode("test"))
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(userOne, userTwo, userThree);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
